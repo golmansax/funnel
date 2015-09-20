b148e5f3050be40903c6dc0535eb835d59f1e031
Holman Gao <holman@golmansax.com>
2015-09-19 23:47:57 -0700

    Woo

--- client/link.js
+++ client/link.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 export default class Link extends React.Component {
   render() {
+    const myTarget = document.domain.indexOf('walmart') >= 0 ? null : '_blank';
     return (
-      <a {...this.props}>
+      <a {...this.props} target={myTarget}>
         {this.props.children}
       </a>
     );
